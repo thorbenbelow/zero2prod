468788c1640eaad800a7894c37c6d7e2b38c017d
Thorben Below <thorbenbelow@gmail.com>
2022-11-24 21:56:57 +0100

    add subscription domain logic

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,10 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+use crate::domain::new_subscriber::NewSubscriber;
+use crate::domain::subscriber_email::SubscriberEmail;
+use crate::domain::subscriber_name::SubscriberName;
+
 #[derive(serde::Deserialize)]
 pub struct SubscriptionFormData {
     name: String,
@@ -22,7 +26,17 @@ pub async fn subscribe(
     form: web::Form<SubscriptionFormData>,
     conn: web::Data<PgPool>,
 ) -> HttpResponse {
-    match insert_subscriber(&form, &conn).await {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    match insert_subscriber(&NewSubscriber { name, email }, &conn).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -30,10 +44,10 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, conn)
+    skip(new_subscriber, conn)
 )]
 pub async fn insert_subscriber(
-    form: &SubscriptionFormData,
+    new_subscriber: &NewSubscriber,
     conn: &PgPool,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
@@ -42,8 +56,8 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(conn)
